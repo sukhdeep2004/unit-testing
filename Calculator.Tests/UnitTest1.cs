40b2a5d522a497c7217f39c120d56b38f1149825
sukhdeep2004 <sukh.d0609@gmail.com>
2025-09-08 13:47:04 -0500

    Add project files.

---
+++ Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Calculator.service;
+
+namespace Prime.UnitTests.Services
+{
+    [TestFixture]
+    public class PrimeService_IsPrimeShould
+    {
+        private Calculate _calc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calc = new Calculate();
+        }
+
+        [Test]
+        public void Addition_test()
+        {
+            var result = _calc.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3), $"Expected {result} to be 3");
+        }
+        [Test]
+        public void Subtraction_test()
+        {
+            var result = _calc.Subtract(5, 2);
+            Assert.That(result, Is.EqualTo(3), $"Expected {result} to be 3");
+        }
+        [Test]
+        public void Multiplication_test()
+        {
+            var result = _calc.Multiply(5, 2);
+            Assert.That(result, Is.EqualTo(10), $"Expected {result} to be 10");
+        }
+        [Test]
+        public void Division_test()
+        {
+            var result = _calc.Divide(10, 2);
+            Assert.That(result, Is.EqualTo(5), $"Expected {result} to be 5");
+        }
+        [Test]
+        public void Division_ByZero_ShouldThrowException()
+        {
+            Assert.Throws<DivideByZeroException>(() => _calc.Divide(5, 0));
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Calculator.service
+{
+    public class Calculate
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Denominator cannot be zero.");
+            }
+            return (double)a / b;
+        }
+        public int Modulus(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Denominator cannot be zero.");
+            }
+            return a % b;
+        }
+        public double Power(int a, int b)
+        {
+            return Math.Pow(a, b);
+        }
+        public double SquareRoot(int a)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Cannot compute square root of a negative number.");
+            }
+            return Math.Sqrt(a);
+        }
+        public double Factorial(int a)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Cannot compute factorial of a negative number.");
+            }
+            if (a == 0 || a == 1)
+            {
+                return 1;
+            }
+            double result = 1;
+            for (int i = 2; i <= a; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
+
+
